12475625e7e9ecf0436a987c849cf7d8fdc6ff30
zohasaleem <zohasaleem2000@gmail.com>
2024-02-21 21:42:23 +0500

    image upload & tinyMCE

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
+
 const admin = require('firebase-admin');
 const serviceAccount = require('./uploadimage-6caaa-firebase-adminsdk-rpkac-d3e875388b.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://uploadimage-6caaa-default-rtdb.firebaseio.com'
+    databaseURL: 'https://uploadimage-6caaa-default-rtdb.firebaseio.com',
+    storageBucket: 'uploadimage-6caaa.appspot.com' 
+
 });
 
 const db = admin.database();
@@ -17,13 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const cmsRouter = require('./routes/cms');
-app.use('/cms', cmsRouter(cmsRef));
+app.use('/cms', cmsRouter(cmsRef, admin));
 
 app.use(express.json());
 
-app.use(express.static('public')); // Serve static files from the 'public' directory
+app.use(express.static('public'));
 
-// Serve cms.html file when accessing /cms.html route
 app.get('/cms.html', (req, res) => {
   res.sendFile(__dirname + '/cms.html');
 });
--- routes/cms.js
+++ routes/cms.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = (cmsRef) => {
+module.exports = (cmsRef, admin) => {
     router.get('/', async(req, res) => {
         try {
             const snapshot = await cmsRef.once('value');
@@ -13,20 +13,72 @@ module.exports = (cmsRef) => {
         }
     });
 
+
+
+
+
     router.post('/', async(req, res) => {
         try {
-            console.log(req.body);
-            const newCmsRef = cmsRef.push();
-            await newCmsRef.set({
-                key: req.body.cmskey,
-                value: req.body.value,
-                type: req.body.type
-            });
+           
+            
+            if (req.files && req.files.image){
+
+                const fileName = file.name;
+        
+                const storageRef = admin.storage().bucket();
+                const imageRef = storageRef.file('node/' + fileName);
+                await imageRef.save(file.data);
+
+                downloadURL = await imageRef.getSignedUrl({
+                    action: 'read',
+                    expires: '03-17-2025' 
+                });
+    
+                const newCmsRef = cmsRef.push();
+                await newCmsRef.set({
+                    key: req.body.cmskey,
+                    value: downloadURL,
+                    type: req.body.type
+                });
+            }
+            else{
+               const newCmsRef = cmsRef.push();
+               await newCmsRef.set({
+                   key: req.body.cmskey,
+                   value: req.body.value,
+                   type: req.body.type
+               });   
+
+            }
+    
             res.json({ message: 'Record created successfully' });
         } catch (error) {
             res.status(500).send('Error: ' + error.message);
         }
     });
+    
+
+
+
+
+
+
+
+
+    // router.post('/', async(req, res) => {
+    //     try {
+    //         console.log(req.body);
+    //         const newCmsRef = cmsRef.push();
+    //         await newCmsRef.set({
+    //             key: req.body.cmskey,
+    //             value: req.body.value,
+    //             type: req.body.type
+    //         });
+    //         res.json({ message: 'Record created successfully' });
+    //     } catch (error) {
+    //         res.status(500).send('Error: ' + error.message);
+    //     }
+    // });
 
 
     router.get('/:id', async (req, res) => {
@@ -38,21 +90,40 @@ module.exports = (cmsRef) => {
             res.status(500).send('Error: ' + error.message);
         }
     });
-    
+
     router.put('/:id', async (req, res) => {
         try {
+            let downloadURL = '';
+    
+            if (req.files && req.files.editImage) {
+                const file = req.files.editImage;
+                const fileName = file.name;
+    
+                const storageRef = admin.storage().bucket();
+                const imageRef = storageRef.file('node/' + fileName);
+                await imageRef.save(file.data);
+    
+                downloadURL = await imageRef.getSignedUrl({
+                    action: 'read',
+                    expires: '03-17-2025' 
+                });
+            }
+    
             const cmsRecordRef = cmsRef.child(req.params.id);
+
             await cmsRecordRef.update({
                 key: req.body.key,
-                value: req.body.value,
+                value: downloadURL || req.body.value, 
                 type: req.body.type
             });
+    
             res.json({ message: 'Record updated successfully' });
+            
         } catch (error) {
             res.status(500).send('Error: ' + error.message);
         }
     });
-
+    
 
     router.delete('/:id', async (req, res) => {
         try {
